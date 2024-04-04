cf57396bf97dc9a72fe9cffe2c9906549b18d58d
TheCoolDrop <vanio.begic123@gmail.com>
2024-04-04 12:54:30 +0200

    Update bootstrap

--- examples/single-cluster/bootstrap.sh
+++ examples/single-cluster/bootstrap.sh
@@ -8,10 +8,10 @@ set -xo
 kind delete cluster
 sleep 2
 kind create cluster --config=kind-config.yaml --image kindest/node:v1.28.7
-kubectl apply -k apps/bootstrap-resources/envs/$3
+kustomize build apps/bootstrap-resources/envs/$3 | kubectl apply -f -
 kubectl create secret generic github-credentials --from-literal=url=https://github.com/thecooldrop --from-literal=username=$1 --from-literal=password=$2 -n argocd-rocket-$3 -o yaml --dry-run=client | kubectl apply -f -
 kubectl label secret github-credentials argocd.argoproj.io/secret-type=repo-creds -n argocd-rocket-$3 --overwrite
-kubectl apply -k apps/argo-cd/envs/$3
+kustomize build apps/argo-cd/envs/$3 | kubectl apply -f -
 kubectl apply -f installations/$3/bootstrap/root.yaml
 sleep 120
 kubectl port-forward svc/argocd-server -n argocd-rocket-$3 8080:80
